3cd18e63deae88216c0775ecd3b8f899dd1b2525
lim-bo <artemshap66@gmail.com>
2023-10-28 22:53:55 +0300

    removed all

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	list "learn.go/datastructures"
+)
+
+func main() {
+	example := list.New()
+	for i := 0; i < 10; i++ {
+		example.Append(i)
+	}
+	example.PrintVals()
+	fmt.Print("\n")
+	testslice := []int{1, 5, 7, 4, 3, 6}
+	example = list.NewFromSlice(testslice)
+	example.PrintVals()
+}
